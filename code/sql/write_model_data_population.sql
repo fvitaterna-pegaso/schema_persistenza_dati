a2c1a05b06aa2e542f53604273eb3b3b0e050557
fvitaterna-pegaso <fabio.vitaterna@studenti.unipegaso.it>
2025-09-07 18:07:57 +0200

    Creazione file write_model_data_population.sql
    Conterrà il popolamento dei dati di test

---
+++ code/sql/write_model_data_population.sql
@@ -0,0 +1,6 @@
+/**********
+Nome Script		write_model_data_population.sql
+Autore			Fabio Vitaterna
+Descrizione		Popolamento dei dati di test del modello
+Note			Se il modello già esiste, è opportuno ricrearlo tramite lo script write_model.sql per evitare duplicazioni di dati
+**********/
